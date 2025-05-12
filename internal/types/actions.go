fd11e335add42fd943643892c60ee7d0174fb6d6
Antoine Debroye <ant.debroye@gmail.com>
2025-05-12 18:23:33 +0200

    refacto now blocks can be inside other blocks

--- internal/types/actions.go
+++ internal/types/actions.go
@@ -1,15 +1,16 @@
 package types
 
 type Action struct {
-	Label   string            `yaml:"label"`
-	Type    string            `yaml:"type"` // "http", "exec", "tts"
-	Method  string            `yaml:"method,omitempty"`
-	URL     string            `yaml:"url,omitempty"`
-	Headers map[string]string `yaml:"headers,omitempty"`
-	Body    string            `yaml:"body,omitempty"`
-	Text    string            `yaml:"text,omitempty"`
-	Command string            `yaml:"command,omitempty"`
-	Args    []string          `yaml:"args,omitempty"`
+	Label    string            `yaml:"label"`
+	Type     string            `yaml:"type"` // "http", "exec", "tts", "container"
+	Method   string            `yaml:"method,omitempty"`
+	URL      string            `yaml:"url,omitempty"`
+	Headers  map[string]string `yaml:"headers,omitempty"`
+	Body     string            `yaml:"body,omitempty"`
+	Text     string            `yaml:"text,omitempty"`
+	Command  string            `yaml:"command,omitempty"`
+	Args     []string          `yaml:"args,omitempty"`
+	Children []Action          `yaml:"children,omitempty"` // Sous-blocs récursifs
 }
 
 type Config struct {
