65953341ba770fa8d46a0a68c2d61cce2ee77ff0
Antoine Debroye <ant.debroye@gmail.com>
2025-09-01 18:55:42 +0200

    fix(tts) show logs made by the rust TTS

--- internal/tts/localRust.go
+++ internal/tts/localRust.go
@@ -16,6 +16,9 @@ func NewRustTTS(path string) TTSProvider {
 func (r *RustTTS) Say(text string) error {
 	cmd := exec.Command(r.binPath, text)
 	output, err := cmd.CombinedOutput()
+	if len(output) > 0 {
+		fmt.Println(string(output)) // on log ce que le binaire dit
+	}
 	if err != nil {
 		return fmt.Errorf("rust-tts error: %w\nOutput: %s", err, string(output))
 	}
